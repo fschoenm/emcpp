8b9f84a391ae03031ab00243480affa62401a8aa
Frank Schoenmann <fschoenm@gmail.com>
2015-12-29 19:44:01 +0100

    Implemented array_size with type deduction.

--- 01_typededuction.cc
+++ 01_typededuction.cc
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <string>
 
-int main() {
-	std::cout << "Hello World!";
+#include <catch.hpp>
+
+// implementation
+template <class T, std::size_t N>
+constexpr std::size_t array_size(T (&)[N]) noexcept {
+	return N;
+}
+
+// test cases
+TEST_CASE("array_size", "[mandatory][types]") {
+	SECTION("basic") {
+		int a0[] = { 1 };
+		CHECK(array_size(a0) == 1);
+
+		int a1[] = { 1, 2, 3 };
+		CHECK(array_size(a1) == 3);
+	}
+
+	SECTION("complex types") {
+		std::string a0[] = { "Hello" };
+		CHECK(array_size(a0) == 1);
+
+		std::string a1[] = { "Hello", "World", "!" };
+		CHECK(array_size(a1) == 3);
+	}
 }
